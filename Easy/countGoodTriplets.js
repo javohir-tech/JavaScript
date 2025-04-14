ff550907e68fc0c9b3afa5f4b0e3169dcd9d410f
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-15 01:47:12 +0500

    upfate

--- Easy/countGoodTriplets.js
+++ Easy/countGoodTriplets.js
@@ -1,23 +1,18 @@
-function countGoodTriplets(arr, a, b, c) {
-    let count = 0;
-
+function countGoodTriple(arr, a, b, c) {
+    let count = 0
     for (let i = 0; i < arr.length - 2; i++) {
-        for (let j = i + 1; j < arr.length - 1; j++) {
-            if (Math.abs(arr[i] = arr[j]) <= a) {
-                for (let k = j + 1; k < arr.length; k++) {
+        for (let j = 1 + i; j < arr.length - 1; j++) {
+            if (Math.abs(arr[i] - arr[j]) <= a) {
+                for (let k = j+1; k < arr.length; k++) {
                     if (Math.abs(arr[j] - arr[k]) <= b && Math.abs(arr[i] - arr[k]) <= c) {
-                        count ++
+                        count++
                     }
                 }
             }
         }
     }
-
     return count
 }
 
-let arr = [3, 0, 1, 1, 9, 7];
-let a = 7, b = 2, c = 3;
-
-console.log(countGoodTriplets(arr, a, b, c))
-
+let arr = [3, 0, 1, 1, 9, 7], a = 7, b = 2, c = 3
+console.log(countGoodTriple(arr, a, b, c))
