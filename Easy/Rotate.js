89ab1e39535e290e186843a672dd9cce650f4f5a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-15 18:45:02 +0500

    upfate

---
+++ Easy/Rotate.js
@@ -0,0 +1,12 @@
+function rotate(nums, k) {
+    for (let i = 0; i < k; i++) {
+        let oxirgi = nums.pop()
+        nums.unshift(oxirgi)
+    }
+}
+
+const nums = [1, 2, 3, 4, 5, 6, 7]
+const k = 3
+rotate(nums, k)
+console.log(nums)
+
