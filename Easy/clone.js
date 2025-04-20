1595686fcac51bdb64d4bfc07c433aa6eb05f7fc
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-21 00:03:52 +0500

    test

--- Easy/clone.js
+++ Easy/clone.js
@@ -1,33 +1,32 @@
-function ListNode(val, next = null) {
+function ListNode(val, naxt = null) {
     this.val = val;
-    this.next = next
+    this.naxt = naxt
 }
-
 
 let list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
 let list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
 
-function mergeTwoList(list1, list2) {
+const mergeTwoLists = (list1, list2) => {
     let dummy = new ListNode(-1);
     let current = dummy;
 
     while (list1 !== null && list2 !== null) {
-        if (list2.val > list1.val) {
-            current.next = list1
-            list1 = list1.next
+        if (list1.val > list2.val) {
+            current.naxt = list2;
+            list2 = list2.naxt
         } else {
-            current.next = list2;
-            list2 = list2.next
+            current.naxt = list1;
+            list1 = list1.naxt;
         }
-        current = current.next
+        current = current.naxt
     }
-
-    current.next = list1 !== null ? list1 : list2;
-    return dummy.next
+    current = list1 !== null ? list1 : list2;
+    return dummy.naxt
 }
 
-let merged = mergeTwoList(list1, list2);
+let merged = mergeTwoLists(list1, list2);
+
 while (merged !== null) {
     console.log(merged.val)
-    merged = merged.next
+    merged = merged.naxt
 }
