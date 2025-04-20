6b0a48b04d76208745e34ed1caca8c9e62796054
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-21 00:49:09 +0500

    update

---
+++ Easy/removeDublicates.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+// nums.splice
+
+var removeDuplicates = function (nums) {
+    if(nums.length === 0) return 0;
+    
+    let k = 1;
+    
+    for (let i = 1; i < nums.length; i++) {
+        if(nums[i]!==nums[i-1]){
+            nums[k] = nums[i];
+            k++
+        }
+    }
+    return k
+};
+
+let nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+let k = removeDuplicates(nums)
+console.log(k)
+console.log(nums.slice(0, k))
