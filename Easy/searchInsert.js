b284196cebb36110ebefe3d8631cf92369247f7d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-22 01:03:46 +0500

    searchInsert.js

---
+++ Easy/searchInsert.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function (nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] === target) {
+            return i
+        }
+    }
+};
+
+const nums = [1, 3, 5, 6]
+console.log(nums.sort())
+const target = 5
+console.log(searchInsert(nums, target))
