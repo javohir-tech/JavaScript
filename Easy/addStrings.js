107cc96fb02ba8a54a087ff54d103cd358371088
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-22 15:33:11 +0500

    update

--- Easy/addStrings.js
+++ Easy/addStrings.js
@@ -4,16 +4,16 @@
  * @return {string}
  */
 var addStrings = function (num1, num2) {
-    let result = ""; // "" "4" "34" "134"
-    let carry = 0; // 0 0 0 
-    let i = num1.length - 1; //1 0 -1
-    let j = num2.length - 1; //2 1 0
+    let result = ""; 
+    let carry = 0;
+    let i = num1.length - 1; 
+    let j = num2.length - 1;
 
     while (i >= 0 || j >= 0 || carry) {
         let sum = carry; // 0  0  0
         if (i >= 0) sum += Number(num1[i]);
         if (j >= 0) sum += Number(num2[j]);
-        result = (sum % 10) + result; // 4 34 134
+        result = (sum % 10) + result; 
         carry = Math.floor(sum / 10)
         console.log(sum, carry)
         i--;
