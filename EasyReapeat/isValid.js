b1b9f366ac9e3730c3ca6d251d3f240e18168f31
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-25 23:56:42 +0500

    update

--- EasyReapeat/isValid.js
+++ EasyReapeat/isValid.js
@@ -7,20 +7,21 @@ function isValid(s) {
     let stack = [];
     const map = {
         ")": "(",
-        "]": "[",
-        "}": "{"
+        "}": "{",
+        "]": "["
     }
+
     for (char of s) {
         if (char === "(" || char === "[" || char === "{") {
-            stack.push(char)                                
-        } else if (stack.length === 0 || stack.pop() !== map[char]) {
+            stack.push(char)
+        }else if(stack.length === 0 || stack.pop() !== map[char]){
             return false
         }
     }
     return stack.length === 0
 }
 
-const s = "([])["
+const s = "([])"
 
 console.log(isValid(s))
 
