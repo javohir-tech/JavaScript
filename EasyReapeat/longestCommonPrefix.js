ba20530c048eb0c313d6eadd28d40624ba1c7de2
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-25 23:40:58 +0500

    update

--- EasyReapeat/longestCommonPrefix.js
+++ EasyReapeat/longestCommonPrefix.js
@@ -4,11 +4,12 @@
  */
 
 function longestCommonPrefix(strs) {
-    let current = strs[0]
+    let current = strs[0];
+
     for (let i = 0; i < strs.length; i++) {
         while (strs[i].indexOf(current) !== 0) {
-            current = current.slice(0, -1)
-            if (current === ' ') return ""
+            current = current.slice(0, -1);
+            if(current === "") return ""
         }
     }
 
@@ -18,3 +19,4 @@ function longestCommonPrefix(strs) {
 let strs = ["flower", "flow", "flight"]
 
 console.log(longestCommonPrefix(strs))
+
