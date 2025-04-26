55572ce2ff0950a2ee14d700b991b9d83330a3ea
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-26 22:30:55 +0500

    mergeTwoLists

--- EasyReapeat/mergeTwoLists.js
+++ EasyReapeat/mergeTwoLists.js
@@ -3,33 +3,38 @@ function ListNode(val, next = null) {
     this.next = next
 }
 
-const mergeGTwoLists = (list1, list2) => {
+const mergeTwoLists = (list1, list2) => {
     let dummy = new ListNode(-1);
     let current = dummy;
 
-    while (list1 != null && list2 !== null) {
-        if (list1.val < list2.val) {
-            current.next = list1;
-            list1 = list1.next
-        } else {
+    while (list1 !== null && list2 !== null) {
+        if (list1.val > list2.val) {
             current.next = list2;
             list2 = list2.next
+        } else {
+            current.next = list1;
+            list1 = list1.next
         }
-
         current = current.next
     }
 
-    current.next = list1 !== null ? list1 : list2;
+    current.next = list1 !== null ? list1 : list2
 
     return dummy.next
 }
 
+
 let list1 = new ListNode(1, new ListNode(2, new ListNode(4)))
 let list2 = new ListNode(1, new ListNode(3, new ListNode(4)))
 
-let merged = mergeGTwoLists(list1, list2)
+// mergeTwoLists(list1, list2)
+let merged = mergeTwoLists(list1, list2)
+
+let result = []
 
 while (merged !== null) {
-    console.log(merged.val)
-    merged = merged.next
+    result.push(merged.val)
+    merged =  merged.next
 }
+
+console.log(result)
