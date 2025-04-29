87d8e5039e224048571ab37653aa167510f101f6
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-04-30 00:44:30 +0500

    update

--- Easy/deleteDublicates.js
+++ Easy/deleteDublicates.js
@@ -1,40 +1,30 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * @param {ListNode} head
- * @return {ListNode}
- */
-
 function ListNode(val, next = null) {
     this.val = val;
     this.next = next
 }
 
-var deleteDuplicates = function (head) {
+const deleteDuplicates = (head) => {
     let current = head;
+
     while (current !== null && current.next !== null) {
-        if (current.val === current.next.val) {
+        if(current.val ===  current.next.val){
             current.next = current.next.next
-        } else {
+        }else{
             current = current.next
         }
+        console.log(current)
     }
 
     return head
-
-};
+}
 
 let head = new ListNode(1, new ListNode(1, new ListNode(2)))
 
-let result = deleteDuplicates(head)
+// console.log(deleteDuplicates(head))
 
-let arr = [];
-while (result !== null) {
+let result = deleteDuplicates(head);
+let arr = []
+while(result!==null){
     arr.push(result.val)
     result = result.next
 }
