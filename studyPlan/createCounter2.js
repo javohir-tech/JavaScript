93934a0e15c5caf1618c0a4c8493b9f5b0764060
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-02 23:27:08 +0500

    update

--- studyPlan/createCounter2.js
+++ studyPlan/createCounter2.js
@@ -23,10 +23,3 @@ console.log(counter.increment());
 console.log(counter.increment());
 console.log(counter.reset());
 console.log(counter.decrement());
-
-/**
- * const counter = createCounter(5)
- * counter.increment(); // 6
- * counter.reset(); // 5
- * counter.decrement(); // 4
- */
