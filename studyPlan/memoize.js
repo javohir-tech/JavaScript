1244ed2361a1752fb615e8a5a59c2fbbbd04567a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-04 19:49:41 +0500

    update

--- studyPlan/memoize.js
+++ studyPlan/memoize.js
@@ -1,42 +1,27 @@
-/**
- * @param {Function} fn
- * @return {Function}
- */
 function memoize(fn) {
-    const cache = {}
+    const kesh = {};
 
     return function (...args) {
-        const key = JSON.stringify(args)
-        if (key in cache) {
-            return cache[key]
+        const key = JSON.stringify(args);
+        if (key in kesh) {
+            return kesh[key]
         }
-        const result = fn(...args);
-        cache[key] = result;
-        return result
+        const natija = fn(...args);
+        kesh[key] = natija
+        return natija
     }
-
 }
 
-let callCount = 0
+let callCount = 0;
 
 const sum = (a, b) => {
-    callCount += 1
-    return a + b
+    callCount += 1;
+    return a + b;
 }
 
-const memoizedFn = memoize(sum)
+const memoizedFn = memoize(sum);
+
 console.log(memoizedFn(3, 2))
 console.log(memoizedFn(3, 2))
+console.log(memoizedFn(1, 3))
 console.log(callCount)
-
-
-/** 
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1 
- */
