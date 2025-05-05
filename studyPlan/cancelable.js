c5c9b1d7332a3496436236ebfe252ad958f781fd
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-05 20:04:22 +0500

    update

--- studyPlan/cancelable.js
+++ studyPlan/cancelable.js
@@ -4,29 +4,30 @@
  * @param {number} t
  * @return {Function}
  */
-var cancellable = function (fn, args, t) {
-    const timer = setTimeout(()=>{
+
+function cencellable(fn, args, t,) {
+    const timer = setTimeout(() => {
         fn(...args)
     }, t)
 
-    return function cencalFn(timer){
+    return function cencelFn() {
         clearTimeout(timer)
     }
-};
+}
 
-const result = []
-const fn = (x) => x * 5;
-const args = [2], t = 20, cancelTimeMs = 50
+let result = [];
+const args = [2, 4], t = 20, cencelTimeMs = 50
+const fn = (x)=> x * 3;
 const start = performance.now()
 const log = (...argsArr) => {
     const diff = Math.floor(performance.now() - start);
-    result.push({"time": diff, "returned": fn(...argsArr)});
+    result.push({ "time": diff, "returned": fn(...argsArr) })
 }
-     
-const cancel = cancellable(log, args, t)
-const maxT = Math.max(t, cancelTimeMs);
-         
-setTimeout(cancel, cancelTimeMs)
+
+const cencel = cencellable(log, args, t)
+const maxT = Math.max(t, cencelTimeMs)
+
+setTimeout(cencel, cencelTimeMs);
 setTimeout(() => {
-    console.log(result); // [{"time":20,"returned":10}]
+    console.log(result)
 }, maxT + 15)
