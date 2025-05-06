16901174e252c54089d66266d6d828fbb9d39334
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-06 23:36:28 +0500

    update

--- studyPLan2/createCounter.js
+++ studyPLan2/createCounter.js
@@ -2,9 +2,28 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function(init) {
-    
+var createCounter = function (init) {
+    let val = init
+    return {
+        increment: function () {
+            val += 1
+            return val
+        },
+        decrement: function () {
+            val-=1
+            return val
+        },
+        reset : function(){
+            val = init;
+            return val
+        }
+    }
 };
+
+const counter = createCounter(5)
+console.log(counter.increment())
+console.log(counter.decrement())
+console.log(counter.reset())
 
 /**
  * const counter = createCounter(5)
