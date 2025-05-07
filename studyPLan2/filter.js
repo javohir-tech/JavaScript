bda37a18652338301a7d3d36417a9af6721427e5
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-07 16:59:12 +0500

    update

--- studyPLan2/filter.js
+++ studyPLan2/filter.js
@@ -1,27 +1,24 @@
-/**
- * @param {number[]} arr
- * @param {Function} fn
- * @return {number[]}
- */
-var filter = function (arr, fn) {
-
-    let newArr = arr.filter((el, i) => fn(el, i) !== false)
-    let result = []
-    for (let i = 0; i < newArr.length; i++) {
-        if (typeof (fn(arr[i], i)) !==Boolean){
-            result.push(fn(newArr[i], i))
+function filter(arr, fn) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            result.push(arr[i])
         }
     }
     return result
-};
-
-const arr = [-2, -1, 0, 1, 2]
-const funk = (n, i) => {
-    if (n !== 0) {
-        return n += 1
-    } else {
-        return false
-    }
 }
-const filtered = filter(arr, funk)
-console.log(filtered)
+
+const fn = (n) => {
+    return n > 10
+}
+
+const firstIndex = (n, i) => {
+    return i === 0
+}
+
+const plusOne = (n) => {
+    return n + 1 !== 0
+}
+const arr = [0, 10, 20, 30]
+
+console.log(filter(arr, fn))
