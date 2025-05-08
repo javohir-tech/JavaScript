0e9c6d6f4051501305979d4c9f6cbe0f0fbe9e48
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-09 00:02:50 +0500

    sort by

--- studyPlan/Objects/sortBy.js
+++ studyPlan/Objects/sortBy.js
@@ -4,13 +4,7 @@
  * @return {Array}
  */
 var sortBy = function (arr, fn) {
-    let result = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        result.push(fn(arr[i]))
-    }
-
-    return result.sort((a, b) => a - b)
+    return arr.sort((a, b) => fn(a) - fn(b))
 };
 
 const arr = [{ "x": 1 }, { "x": 0 }, { "x": -1 }]
