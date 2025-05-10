cadc5963b8b6c40921e02928132ac58257f0421a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-11 00:00:23 +0500

    update

--- studyPlan/Objects/flat.js
+++ studyPlan/Objects/flat.js
@@ -4,12 +4,15 @@
  * @return {Array}
  */
 var flat = function (arr, n) {
-    
+    const result = arr.flat(n);
+    return result
 };
 
 
 
 const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
 let n = 0
+
+console.log(flat(arr, n))
 
 
