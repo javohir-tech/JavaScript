83a655cb1955092e5696d17d2c9bb6ff6f109bf7
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-10 23:38:25 +0500

    start

---
+++ studyPlan/Objects/flat.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Array} arr
+ * @param {number} depth
+ * @return {Array}
+ */
+var flat = function (arr, n) {
+    
+};
+
+
+
+const arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+let n = 0
+
+
