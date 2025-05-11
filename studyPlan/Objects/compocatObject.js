974d6db0749a416ef58fc18d36312375a2566395
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-11 23:34:36 +0500

    update

--- studyPlan/Objects/compocatObject.js
+++ studyPlan/Objects/compocatObject.js
@@ -6,15 +6,15 @@ function compactObject(obj) {
     } else if (obj !== null && typeof obj === 'object') {
         const result = {};
         for (let key in obj) {
-            const compacted = compactObject(obj[key]); // chuqur tozalash uchun 
-            
+            const compacted = compactObject(obj[key]); // chuqur tozalash uchun  // null , [false , 1] => [1]
+            console.log(obj[key])
             if (
                 (typeof compacted === 'object' && compacted !== null && Object.keys(compacted).length > 0) ||
                 (typeof compacted !== 'object' && Boolean(compacted))
             ) {
                 result[key] = compacted;
             }
-            console.log(compacted)
+            // console.log(compacted)
 
         }
         return result;
@@ -24,5 +24,5 @@ function compactObject(obj) {
 }
 
 // console.log(compactObject([null, 0, false, 1])); 
-console.log(compactObject({"a": null, "b": [false, 1]})); 
+console.log(compactObject({ "a": null, "b": [false, 1] }));
 // console.log(compactObject([null, 0, 5, [0], [false, 16]])); 
