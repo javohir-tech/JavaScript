a8ff6a8e0fd03108fac1d1df95141ea7f950a54e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-14 23:44:24 +0500

    update

--- studyPlan3/expect.js
+++ studyPlan3/expect.js
@@ -2,9 +2,26 @@
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
-    
+var expect = function (val) {
+    return {
+        toBe: function (another) {
+            if (another === val) {
+                return true
+            } else {
+                throw new Error("Not Equal")
+            }
+        },
+        notToBe: function (another) {
+            if (another !== val) {
+                return true
+            } else {
+                throw new Error('Equal')
+            }
+        }
+    }
 };
+
+console.log(expect(5).toBe(5))
 
 /**
  * expect(5).toBe(5); // true
