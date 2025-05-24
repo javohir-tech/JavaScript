19cac05c1a7beb1544cd0030668a8096ee8afb3c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-24 23:40:53 +0500

    update

--- studyPlan3/sortBy.js
+++ studyPlan3/sortBy.js
@@ -4,9 +4,13 @@
  * @return {Array}
  */
 var sortBy = function (arr, fn) {
-
+    return arr.sort((a, b)=>fn(a)-fn(b))
 };
 
-const fn = (x) => {
-    return x
+const arr = [{ "x": 1 }, { "x": 0 }, { "x": -1 }]
+
+const fn = (d) => {
+    return d.x
 }
+
+console.log(sortBy(arr, fn))
