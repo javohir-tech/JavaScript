a33da166fff8e1fba3f5fa1df3d2ad6bb3ff26c8
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-27 21:05:57 +0500

    update

--- leetCode75/string/prodduct.js
+++ leetCode75/string/prodduct.js
@@ -4,20 +4,19 @@
  */
 var productExceptSelf = function (nums) {
     const n = nums.length;
-    const result = Array(n).fill(1);
+    const natija = Array(n).fill(1);
 
-    let prefix = 1;
+    let oldingi = 1;
     for (let i = 0; i < n; i++) {
-        result[i] = prefix;
-        prefix *= nums[i]
+        natija[i] = oldingi;
+        oldingi *= nums[i]
     }
-    console.log(result)
-    let suffix = 1;
-    for(let i=n-1 ; i>=0 ; i--){
-        result[i] = suffix;
-        suffix  *= nums[i]
+    let keyingi = 1;
+    for (let i = n - 1; i >= 0; i--) {
+        natija[i] = keyingi ;
+        keyingi*=  nums[i]
     }
-    return result
+    return natija
 };
-const nums = [1, 2, 3, 4, 5]
+const nums = [1, 2, 3, 4]
 console.log(productExceptSelf(nums))
