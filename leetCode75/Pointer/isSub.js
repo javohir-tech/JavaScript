fdea39d161bd689b1c1b29bb678024d05de801a4
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-05-29 22:17:06 +0500

    update

--- leetCode75/Pointer/isSub.js
+++ leetCode75/Pointer/isSub.js
@@ -4,11 +4,16 @@
  * @return {boolean}
  */
 var isSubsequence = function (s, t) {
-    let pattern  = s.split('').join('.*');
-    let regex = new RegExp(pattern);
-    return regex.test(t)
+    if(s.length === 0) return true;
+    if(t.length === 0) return false;
+
+    if(s[0]===t[0]){
+        return isSubsequence(s.slice(1) , t.slice(1))
+    }else{
+        return isSubsequence(s, t.slice(1));
+    }
 }
 
 const s = "abc"
-const t = "ahbgdc"
+const t = "ahxgdc"
 console.log(isSubsequence(s, t))
