132ee7087fb33c930d3eae8bb0bcbf38c7e02e39
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-02 22:35:17 +0500

    update

--- leetCode75/Pointer/maxArea.js
+++ leetCode75/Pointer/maxArea.js
@@ -1,25 +1,24 @@
 function maxArea(height) {
-  let left = 0;
-  let right = height.length - 1;
-  let maxArea = 0;
+    let left = 0;
+    let right = height.length - 1;
+    let maxArea = 0;
 
-  while (left < right) {
-    const heightMin = Math.min(height[left], height[right]);
-    const width = right - left;
-    const area = heightMin * width;
-    maxArea = Math.max(maxArea, area);
+    while (left < right) {
+        const minHeight = Math.min(height[left] , height[right]);
+        const width = right-left;
+        const area = minHeight*width;
+        maxArea = Math.max(area , maxArea);
 
-    // Kichikroq balandlikni ichkariga siljitamiz
-    if (height[left] < height[right]) {
-      left++;
-    } else {
-      right--;
+        if(height[left] < height[right]){
+            left++
+        }else{
+            right--
+        }
     }
-  }
-
-  return maxArea;
+    return maxArea
 }
 
 // Misollar
-console.log(maxArea([1,8,6,2,5,4,8,3,7])); // 49
-console.log(maxArea([1,1]));               // 1
+console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])); // 49
+console.log(maxArea([1, 1]));
+Array.length               // 1
