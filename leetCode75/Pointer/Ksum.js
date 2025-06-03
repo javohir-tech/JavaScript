7dbfcc5b686640c863a329ce3a82441617d9a343
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-03 11:36:24 +0500

    update

--- leetCode75/Pointer/Ksum.js
+++ leetCode75/Pointer/Ksum.js
@@ -1,22 +1,20 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
 var maxOperations = function (nums, k) {
+    const map = new Map();
     let count = 0;
-    let left = 0;
-    let rigth = nums.length;
-    while (left < nums.length) {
-        if (nums[left] + nums[rigth] === k) {
-            nums.splice(rigth , 1)
-            nums.splice(left , 1)
-        }else{
+    for (let num of nums) {
+        const diff = k - num;
 
+        if (map.get(diff) > 0) {
+            count++;
+            map.set(diff, map.get(diff) - 1)
+        } else {
+            map.set(num, (map.get(num) || 0) + 1)
         }
     }
+
+    return count
 };
 
-const nums = [1, 2, 3, 4]
-const k = 5
-console.log(maxOperations(nums, k))
+const nums = [1, 2, 3, 4];
+const k = 5;
+console.log(maxOperations(nums, k));  // Chiqish: 1
