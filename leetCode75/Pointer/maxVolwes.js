f7831e772e2e606ef6cdfe55d0be301ec5dcf404
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-10 16:23:15 +0500

    update

--- leetCode75/Pointer/maxVolwes.js
+++ leetCode75/Pointer/maxVolwes.js
@@ -10,7 +10,7 @@ var maxVowels = function (s, k) {
     for (let i = 0; i < s.length; i++) {
         if (vowles.has(s[i])) count++;
         if (i >= k && vowles.has(s[i - k])) count--;
-        if (i => k - 1) max= Math.max(max , count)
+        if (i >= k - 1) max = Math.max(max, count)
     }
     return max
 };
