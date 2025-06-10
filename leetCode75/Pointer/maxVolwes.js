0339e2b1c0d0d629075395ae743ed9f52abaf9c8
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-10 15:49:49 +0500

    update

--- leetCode75/Pointer/maxVolwes.js
+++ leetCode75/Pointer/maxVolwes.js
@@ -4,16 +4,15 @@
  * @return {number}
  */
 var maxVowels = function (s, k) {
-    const vowels = new Set(['a', 'e', 'i', 'o', 'u']);
-    let count = 0, max = 0;
-
+    const vowels = new Set('a', 'e', 'i', 'o', 'u');
+    let count = 0;
+    let max = 0;
     for (let i = 0; i < s.length; i++) {
         if (vowels.has(s[i])) count++;
-        if (i >= k && vowels.has(s[i - k])) count--; // oldindan chiqqan harfni ayiramiz
-        if (i >= k - 1) max = Math.max(max, count);  // faqat to'liq oynalar uchun solishtiramiz
+        if (i >= k && vowels.has(s[i - k])) count--;
+        if (i >= k - 1) max = Math.max(max, count)
     }
-
-    return max;
+    return max
 };
 
 const s = "leetcode";
