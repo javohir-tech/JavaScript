1178163017b45d43f1801e7cad94b04862321b4f
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-10 17:13:30 +0500

    update

---
+++ leetCode75/Pointer/longestOnes.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function (nums, k) {
+    
+};
+
+const nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
+const k = 2
--- leetCode75/Pointer/maxVolwes.js
+++ leetCode75/Pointer/maxVolwes.js
@@ -7,11 +7,13 @@ var maxVowels = function (s, k) {
     const vowles = new Set(['a', 'u', 'o', 'i', 'e'])
     let count = 0;
     let max = 0;
+    
     for (let i = 0; i < s.length; i++) {
         if (vowles.has(s[i])) count++
         if (i >= k && vowles.has(s[i - k])) count--;
         if (i >= k - 1) max = Math.max(max, count)
     }
+
     return max
 };
 
