2f9622d867192416d5e6c9e221f1a5ce4146e2cd
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-11 13:37:16 +0500

    updete

--- leetCode75/Pointer/longestOnes.js
+++ leetCode75/Pointer/longestOnes.js
@@ -4,16 +4,16 @@
  * @return {number}
  */
 var longestOnes = function (nums, k) {
-    let max = 0;
-    for (let i = 0; i < nums.length; i++) {
-        let count = 0;
-        for (let j = i; j < nums.length; j++) {
-            if (nums[j] === 0) count++;
-            if (count > k) break;
-            max = Math.max(max, j - i + 1)
+    let left = 0;
+    for (let right = 0; right < nums.length; right++) {
+        if (nums[right] === 0) k--;
+
+        if (k < 0) {
+            if (nums[left] === 0) k++;
+            left++;
         }
     }
-    return max
+    return nums.length-left;
 };
 
 const nums = [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0]
