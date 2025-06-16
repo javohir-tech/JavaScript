2679bef020e06814240b2abc22731cca68339388
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-06-16 14:04:36 +0500

    update

---
+++ repeat/timeLImit.js
@@ -0,0 +1,53 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function (fn, t) {
+    return async function (...args) {
+        return new Promise((resolve, reject) => {
+            let isResolved = false
+            fn(...args)
+                .then(res => {
+                    if (!isResolved) {
+                        isResolved = true
+                        resolve(res)
+                    }
+                }).catch(err => {
+                    if (!isResolved) {
+                        isResolved = true;
+                        reject(err)
+                    }
+                })
+
+            setTimeout(()=>{
+                if(!isResolved){
+                    reject("Time Limit Exceeded")
+                    isResolved = true
+                }
+            } , t)
+        })
+    }
+};
+
+
+const fn = async (n) => {
+    await new Promise(resolve => setTimeout(resolve, 100))
+    return n * n;
+};
+const start = Date.now()
+
+t = 10;
+
+const func = timeLimit(fn, t)
+func(4)
+    .then((res) => {
+        console.log({ "returned": res, "Time": Math.floor(Date.now() - start) })
+    }).catch(err => {
+        console.log({ "error": err, "Time": Math.floor(Date.now() - start) })
+    })
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
