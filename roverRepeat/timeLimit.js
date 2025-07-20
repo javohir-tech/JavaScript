d7c092f8e1a8f91fdd50e84da43e2d163f42047d
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-20 22:41:42 +0500

    update

--- roverRepeat/timeLimit.js
+++ roverRepeat/timeLimit.js
@@ -6,16 +6,38 @@
 var timeLimit = function (fn, t) {
 
     return async function (...args) {
-
+        return new Promise((resolve, reject) => {
+            fn(...args)
+                .then(res => {
+                    resolve(res)
+                })
+            setTimeout(() => {
+                reject("time limit exceeded")
+            }, t)
+        })
     }
-};
 
-const fn = (t) => {
-    new Promise(res => setTimeout(res, t))
+};
+let result = []
+const fn = async (n) => {
+    await new Promise(res => setTimeout(res, 100))
+    return n * n
 }
 
-const limited = timeLimit()
+const start = performance.now()
 
+const args = [5];
+
+const limited = timeLimit(fn, 150);
+
+try {
+    const res = await limited(args)
+    result =  {"returned" :  res , "time" : Math.floor(performance.now()-start)}
+} catch (error) {
+    result =  {"rejected" :  error , "time" : Math.floor(performance.now()-start)}
+}
+
+console.log(result)
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
  * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
