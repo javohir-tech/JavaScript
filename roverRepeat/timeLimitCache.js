93909f4821aea306ed1f8145b3ae432f9cd9ee05
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-21 23:18:20 +0500

    update

--- roverRepeat/timeLimitCache.js
+++ roverRepeat/timeLimitCache.js
@@ -6,16 +6,16 @@ class TimeLimitCache {
     }
 
     set(key, value, duration) {
-        if (!this.map.has(key)) {
+
+        const res =  this.map.has(key) && this.map.get(key).time > duration
+
             this.map.set(key,
                 {
                     value,
                     "time": Date.now() + duration
                 }
             )
-            return false
-        }
-        return this.map.get(key)
+        return res
     }
 
     get(key) {
@@ -24,27 +24,32 @@ class TimeLimitCache {
             const timer = Date.now();
             if (res.time > timer) {
                 return res.value
-            } return -1
+            } 
         }
-        return null
+        return -1
     }
 
     count() {
         const timer = Date.now();
         const args = this.map.entries();
-        let sanoq = 0
-        args.map((_, obj) => {
+        let sanoq = 0;
+        for (const [_, obj] of this.map.entries()) {
             if (obj.time > timer) {
                 sanoq++
             }
-        })
+        }
         return sanoq
     }
 }
 
 const timeLimit = new TimeLimitCache()
 console.log(timeLimit.set(1, 42, 1000))
-console.log(timeLimit.get(1))
-console.log(timeLimit.count())
+console.log(timeLimit.set(2, 42, 1000))
+setTimeout(()=>{
+    console.log(timeLimit.get(1))
+} , 500);
+setTimeout(()=>{
+    console.log(timeLimit.count())
+} , 200)
 
 // console.log(Date.now())
