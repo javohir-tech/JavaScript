bdb56d216ac372cc1e9855f8e31727236e4f721b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-22 23:12:20 +0500

    update

--- roverRepeat/promiseAll.js
+++ roverRepeat/promiseAll.js
@@ -3,22 +3,35 @@
  * @return {Promise<any>}
  */
 var promiseAll = function (functions) {
-
-
-
+    return async function () {
+        let count = 0;
+        const result = [];
+        const start = Date.now()
+        return new Promise((resolve, reject) => {
+            functions.forEach((fn, i) => {
+                fn().then((res) => {
+                    result[i] = res
+                    count += 1
+                    if (count === functions.length) {
+                        resolve({ "t": Date.now() - start, "returned": result })
+                    }
+                })
+                    .catch(() => {
+                        reject({ "t": Date.now() - start, "rejected": "error" })
+                    })
+            })
+        })
+    }
 };
 
 const functions = [
-    new Promise(resolve => setTimeout(() => resolve(4), 50)),
-    new Promise(resolve => setTimeout(() => resolve(10), 150)),
-    new Promise(resolve => setTimeout(() => resolve(16), 10))
+   () => new Promise(resolve=> setTimeout(()=>resolve(5) , 200))
 ]
 
 const promise = promiseAll(functions);
-promise()
-.then(res=>{
+promise().then(res => {
     console.log(res)
-}).catch(err=>{
+}).catch(err => {
     console.log(err)
 })
 
