39db558c8bc621df9fa679eba3345584d7b06772
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-23 23:03:05 +0500

    update

---
+++ roverRepeat/chunk.js
@@ -0,0 +1,23 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function (arr, size) {
+    const newArr = [];
+
+    for (let i = 0; i < arr.length; i += size) {
+        let sectionArr = [];
+        console.log(i)
+        for (let j = i; j < size+i; j++) {
+            sectionArr.push(arr[j])
+        }
+        newArr.push(sectionArr)
+    }
+     return newArr
+};
+
+
+const arr = [1, 2, 3, 4, 5];
+const size = 2
+console.log(chunk(arr, size))
