e2b2ecc8ea12b68b18e0356c7791405e382bb437
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-25 00:19:36 +0500

    update

--- roverRepeat/groupBy.js
+++ roverRepeat/groupBy.js
@@ -3,15 +3,33 @@
  * @return {Object}
  */
 Array.prototype.groupBy = function (fn) {
+    const result = {};
 
+    this.forEach((elem) => {
+        const item = fn(elem)
+
+        if (!(item in result)) {
+            result[item] = [elem]
+        }
+        else {
+            result[item].push(elem)
+        }
+    })
+
+    return result
 };
 
+const fn = (elem) => {
+    return elem.id
+}
 
 massiv = [
     { "id": "1" },
     { "id": "1" },
     { "id": "2" }
 ]
+
+console.log(massiv.groupBy(fn))
 
 /**
  * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
