3eb13b1e6884fb760d03398d4ca82d80445a3b3a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-25 22:27:06 +0500

    update

---
+++ roverRepeat/join.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function (arr1, arr2) {
+
+};
+
+arr1 = [
+    { "id": 1, "x": 1 },
+    { "id": 2, "x": 9 }
+],
+    arr2 = [
+        { "id": 3, "x": 5 }
+    ]
+
+console.log(join(arr1 , arr2))
