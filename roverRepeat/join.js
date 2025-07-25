ba63b7f0a214c9ee18a7445e162e88c109915218
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-25 22:52:50 +0500

    updfate

--- roverRepeat/join.js
+++ roverRepeat/join.js
@@ -4,15 +4,32 @@
  * @return {Array}
  */
 var join = function (arr1, arr2) {
+    const res = new Map()
+
+    arr1.forEach(item => {
+        if (!res.has(item.id)) {
+            res.set(item.id, { ...item })
+        }
+    })
+
+    arr2.forEach(item => {
+        if (!res.has(item.id)) {
+            res.set(item.id, { ...item })
+        }
+        else {
+            res.set(item.id , {...res.get(item.id) , ...item})
+        }
+    })
+
+    return res
 
 };
 
 arr1 = [
-    { "id": 1, "x": 1 },
-    { "id": 2, "x": 9 }
-],
-    arr2 = [
-        { "id": 3, "x": 5 }
-    ]
+    {"id": 1, "b": {"b": 94},"v": [4, 3], "y": 48}
+]
+arr2 = [
+    {"id": 1, "b": {"c": 84}, "v": [1, 3]}
+]
 
-console.log(join(arr1 , arr2))
+console.log(join(arr1, arr2))
