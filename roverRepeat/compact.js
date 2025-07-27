aa721532cd2d91c227a616777377bb7b91bde2b0
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-07-27 22:29:41 +0500

    update

--- roverRepeat/compact.js
+++ roverRepeat/compact.js
@@ -3,13 +3,15 @@
  * @return {Object|Array}
  */
 var compactObject = function (obj) {
-
-    if (typeof obj === "object") {
-        const result = {};
-
-        obj.forEach(item => {
-
-        });
+    if (Array.isArray(obj)) {
+        return  obj
+        .map(compactObject)
+        .filter(Boolean)
     }
-
 };
+
+const obj = { "a": null, "b": [false, 1] };
+
+for (let key in obj) {
+    console.log(key)
+}
