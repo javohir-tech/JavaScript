7d5461f471df68befd3ebfa377705824d861c287
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-01 21:34:39 +0500

    update

---
+++ leetCode752/kids.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} candies
+ * @param {number} extraCandies
+ * @return {boolean[]}
+ */
+var kidsWithCandies = function (candies, extraCandies) {
+    let maxNum = 0;
+    const result = []
+    candies.forEach(item => {
+        if (item > maxNum) {
+            maxNum = item;
+        }
+    })
+
+    candies.forEach(item => {
+        result.push(item + extraCandies >= maxNum);
+    })
+
+    return result   
+};
+
+const candies = [2, 3, 5, 1, 3];
+const extraCandies = 3;
+console.log(kidsWithCandies(candies, extraCandies))
