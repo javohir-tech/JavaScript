c4a849b52deae1d26e2c7fcda6b49af4cd16768a
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-01 22:26:43 +0500

    update

--- leetCode752/canPlace.js
+++ leetCode752/canPlace.js
@@ -4,23 +4,22 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function (flowerbed, n) {
-    let bug = []
-    let sum = 0
+    let count = 0;
     for (let i = 0; i < flowerbed.length; i++) {
-        if (flowerbed[i] !== 0) {
-            bug = []
-        } else {
-            bug.push(flowerbed[i])
-            if (bug.length === 3){
-                bug = [0];
-                sum++
-            }
+        if (
+            flowerbed[i] === 0 &&
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1;
+            count ++
         }
     }
-    return sum===n
+
+    return count >= n
 };
 
-const flowerbed = [0,0,1,0,1];
+const flowerbed = [0, 0, 1, 0, 1];
 const n = 1;
 
 console.log(canPlaceFlowers(flowerbed, n))
