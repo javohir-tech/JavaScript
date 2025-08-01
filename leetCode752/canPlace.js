deceef54962445065de3edcfcc80a2e788b27ed6
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-01 21:51:06 +0500

    update

--- leetCode752/canPlace.js
+++ leetCode752/canPlace.js
@@ -3,6 +3,11 @@
  * @param {number} n
  * @return {boolean}
  */
-var canPlaceFlowers = function(flowerbed, n) {
+var canPlaceFlowers = function (flowerbed, n) {
     
 };
+
+const flowerbed = [1, 0, 0, 0, 1];
+const n = 1;
+
+console.log(canPlaceFlowers(flowerbed, n))
