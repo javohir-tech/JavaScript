74d67ee5cbaad6e4c16e4211bf07fc6652e9747b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-05 21:17:41 +0500

    update

--- leetCode752/triple.js
+++ leetCode752/triple.js
@@ -3,7 +3,22 @@
  * @return {boolean}
  */
 var increasingTriplet = function (nums) {
-    
+
+    let boshlangich = nums[0];
+    let result = []
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] > boshlangich) {
+            result.push(boshlangich);
+            // boshlangich = nums[i];
+            console.log(result)
+        } else if (nums[i] < boshlangich) {
+            result = []
+        }
+        boshlangich = nums[i]
+        // console.log(boshlangich)
+    }
+    return result
 };
 
 const nums = [2, 1, 5, 0, 4, 6];
+console.log(increasingTriplet(nums))
