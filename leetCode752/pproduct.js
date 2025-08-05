0743b5d4569f13addb4c089abfe598f3663129b8
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-08-05 20:53:25 +0500

    update

--- leetCode752/pproduct.js
+++ leetCode752/pproduct.js
@@ -10,16 +10,16 @@ var productExceptSelf = function (nums) {
     let prefix = 1;
     for (let i = 0; i < n; i++) {
         result[i] = prefix;
-        prefix *= nums[i];
+        prefix *= nums[i]
     }
 
     let suffix = 1;
     for (let i = n - 1; i >= 0; i--) {
-        result[i] *= suffix;
-        suffix *= nums[i]
+        result[i] *= suffix ;
+        suffix *= nums[i] 
     }
-
-    return result
+    
+    return result;
 };
 
 const nums = [1, 2, 3, 4];
