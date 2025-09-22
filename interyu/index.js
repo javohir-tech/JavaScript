2d4b2f2c6d3f61b942768fa4722e2c459dec8619
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-22 20:43:18 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,7 +1,9 @@
 // let myAge = 21;
 
-const user = { name: "Josh", address: { country: "Uzb", city: "Tashkent" } };
+let user = { name: "Josh", address: { country: "Uzb", city: "Tashkent" } };
 
-const {name , address : {country , city}} = user
+console.log(user.name)
 
-console.log(city)
+user = null
+
+console.log(user.name)
