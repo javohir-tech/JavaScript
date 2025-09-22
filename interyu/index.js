1d6db61bfa9404c329f41e9ff18c63bce0df4572
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-22 21:39:19 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,9 +1,14 @@
-// let myAge = 21;
+const myMethods = {
+    printName() {
+        console.log("Employee's name is : " + this.name)
+    },
+    printPosition() {
+        console.log("Employee's position is : " + this.position)
+    }
+}
 
-let user = { name: "Josh", address: { country: "Uzb", city: "Tashkent" } };
+const emp1 = { name: "Javohir", position: "front end developer" }
+const emp2 = { name: "Jalol", position: "Marksheydr" }
 
-console.log(user.name)
-
-user = null
-
-console.log(user.name)
+myMethods.printName.call(emp1)
+myMethods.printPosition.call(emp1)
