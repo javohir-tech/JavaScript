21721387192af5a5d2d2227f2a9d49301bf86ab3
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-23 00:45:45 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,11 +1,1 @@
-const arr = [1, 2, 3]
-
-// arr.forEach( async (el) => {
-//     await new Promise(r=>setTimeout(r , 1000))
-//     console.log(el)
-// })
-
-for(const el of arr){
-    await new Promise(r=>setTimeout(r, 1000));
-    console.log(el)
-}
+decodeURI("%")
