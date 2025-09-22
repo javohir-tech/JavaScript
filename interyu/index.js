23ba7d71ed3555a4b06569da41c1c426df6bd8d4
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-23 00:02:53 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,30 +1,11 @@
-const apiUrl = "https://jsonplaceholder.typicode.com/users";
+const arr = [1, 2, 3]
 
-function getUsers(method, url, body) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
+// arr.forEach( async (el) => {
+//     await new Promise(r=>setTimeout(r , 1000))
+//     console.log(el)
+// })
 
-        xhr.open(method, url);
-        xhr.onload = function () {
-            if (this.status >= 200 && this.status < 300) resolve(xhr.response);
-            else reject({ status: this.status, statusText: this.statusText });
-        }
-
-        xhr.onerror = function () {
-            reject({
-                status: this.status,
-                statusText: this.statusText
-            })
-        }
-
-        xhr.send(body)
-    })
+for(const el of arr){
+    await new Promise(r=>setTimeout(r, 1000));
+    console.log(el)
 }
-
-getUsers('GET', apiUrl)
-    .then(res => {
-        console.log(res)
-    })
-    .catch(err => {
-        console.log(err)
-    })
