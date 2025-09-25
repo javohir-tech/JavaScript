48e6f9fc1296f112ba4d5f517430627d9aba9cd1
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-26 01:33:30 +0500

    callback async

--- interyu/index.js
+++ interyu/index.js
@@ -1,17 +1,30 @@
-const request = new XMLHttpRequest()
+function getUsers(callback) {
+    const request = new XMLHttpRequest()
 
-request.addEventListener('readystatechange', () => {
-    // console.log(request, request.readyState)
-    if (request.readyState === 4) {
-        console.log(request.responseText)
+    request.addEventListener('readystatechange', () => {
+        // console.log(request, request.readyState)
+        if (request.readyState === 4 && request.status === 200) {
+            callback(request.responseText, undefined)
+        } else if (request.readyState === 4) {
+            callback(undefined, "Ma'lumot olishni iloji bo'lmadi ")
+        }
+    })
+
+    request.open('GET', "https://jsonplaceholder.typicode.com/users")
+
+    request.send()
+}
+
+console.log(1)
+console.log(2)
+getUsers((data, err) => {
+    console.log("callbackishga tushdi")
+    if (data) {
+        console.log(data)
+    } else {
+        console.log(err)
     }
 })
+console.log(3)
+console.log(4)
 
-request.open('GET', "https://jsonplaceholder.typicode.com/users")
-
-request.send()
-
-// 1 open tayyor
-// 2 send qilindi
-// 3 loading...
-// 4 tayyor
