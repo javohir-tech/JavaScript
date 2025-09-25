11acd1abaa4dd7ac74e77c9eb4290d653a17d9c8
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-25 21:27:42 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,13 +1,36 @@
-const person = {
-    isHuman : false,
-    printName(){
-        console.log(`My name is${this.name} . Am i human ? : ${this.isHuman}`)
-    }
+// Shape - superclass
+function Shape() {
+  this.x = 0;
+  this.y = 0;
 }
 
-const javohir = Object.create(person);
+// superclass method
+Shape.prototype.move = function (x, y) {
+  this.x += x;
+  this.y += y;
+  console.info("Shape moved.");
+};
 
-javohir.name = "Javohir";
-javohir.isHuman = true;
+// Rectangle - subclass
+function Rectangle() {
+  Shape.call(this); // call super constructor.
+}
 
-javohir.printName()
+// subclass extends superclass
+Rectangle.prototype = Object.create(Shape.prototype, {
+  // If you don't set Rectangle.prototype.constructor to Rectangle,
+  // it will take the prototype.constructor of Shape (parent).
+  // To avoid that, we set the prototype.constructor to Rectangle (child).
+  constructor: {
+    value: Rectangle,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  },
+});
+
+const rect = new Rectangle();
+
+console.log("Is rect an instance of Rectangle?", rect instanceof Rectangle); // true
+console.log("Is rect an instance of Shape?", rect instanceof Shape); // true
+rect.move(1, 1); // Logs 'Shape moved.'
