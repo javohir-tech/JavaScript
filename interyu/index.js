12494d040dc1cf09945a9b0110db2bff6e111a70
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-26 02:05:56 +0500

    promise

--- interyu/index.js
+++ interyu/index.js
@@ -1,28 +1,39 @@
-function getUsers(apiUrl, callback) {
-    const request = new XMLHttpRequest()
+function getUsers(apiUrl) {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest()
 
-    request.addEventListener('readystatechange', () => {
-        // console.log(request, request.readyState)
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText)
-            callback(data, undefined)
-        } else if (request.readyState === 4) {
-            callback(undefined, "Ma'lumot olishni iloji bo'lmadi ")
-        }
+        request.addEventListener('readystatechange', () => {
+            // console.log(request, request.readyState)
+            if (request.readyState === 4 && request.status === 200) {
+                const data = JSON.parse(request.responseText)
+                resolve(data)
+            } else if (request.readyState === 4) {
+                reject("Ma'lumot olishni iloji bo'lmadi ")
+            }
+        })
+
+        request.open('GET', apiUrl)
+
+        request.send()
     })
 
-    request.open('GET', apiUrl)
-
-    request.send()
 }
 
-getUsers("./Todos/user.json", (data, err) => {
-    console.log(data , "user")
-    getUsers("./Todos/javohir.json", (data, err) => {
-        console.log(data ,"javohir")
-        getUsers("./Todos/suvonov.json", (data, err) => {
-            console.log(data, "suvonov")
-        })
+getUsers("./Todos/user.json")
+    .then(res => {
+        console.log(res)
     })
-})
+    .catch(err => {
+        console.log(err)
+    })
+
+// getUsers("./Todos/user.json", (data, err) => {
+//     console.log(data, "user")
+//     getUsers("./Todos/javohir.json", (data, err) => {
+//         console.log(data, "javohir")
+//         getUsers("./Todos/suvonov.json", (data, err) => {
+//             console.log(data, "suvonov")
+//         })
+//     })
+// })
 
