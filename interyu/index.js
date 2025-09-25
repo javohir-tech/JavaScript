6d8f739ae2acc791e00d644c7f0cadd5d44f5ea0
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-25 19:37:58 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,19 +1,18 @@
-const user = {
-    get name(){
-       return this._name
-    },
-
-    set name(value){
-        if(value.length< 4){
-            console.log("this name too short ");
-            return
-        }
-        this._name =  value;
-    }
+function user(name, age) {
+    this.name = name;
+    this.age = age
 }
 
-// user.name = "Javohir";
-// console.log(user.name);
-user.name = "";
-console.log(user.name)
+const javohir = new user("Javohir", new Date(2004, 4, 29));
 
+console.log(javohir)
+
+Object.defineProperty(this, "age", {
+    get() {
+        let today = new Date().getFullYear();
+        return  today - this.age.getFullYear()
+    }
+})
+
+console.log(javohir.name)
+console.log(javohir.age)
