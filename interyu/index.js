72879195dc03ec826328ff46abfdaa5bb0a43b63
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-25 19:29:41 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,24 +1,19 @@
 const user = {
-    name: "Javohir",
-    firstName: "Suvonov"
+    get name(){
+       return this._name
+    },
+
+    set name(value){
+        if(value.length< 4){
+            console.log("this name too short ");
+            return
+        }
+        this._name =  value;
+    }
 }
 
-Object.defineProperty(user, "fullName", {
-    get() {
-        return `${this.name} ${this.firstName}`
-    },
-    set(value) {
-        [this.name, this.firstName] = value.split(" ")
-    },
-    writable: true, // o'zgartirsa bo'ladi 
-    enumerable: true, // for...in orqali ko'rinadi
-    configurable: false // o'chirip bo'lmaydi  
-})
+// user.name = "Javohir";
+// console.log(user.name);
+user.name = "";
+console.log(user.name)
 
-console.log(user.fullName)
-
-user.fullName = "ulug'bek Suvonov"
-
-for(let key in user){
-    console.log(key + ":" + user[key])
-}
