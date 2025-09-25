bd55a822b7ad815119deb6f350fe2f6759af4103
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-25 19:16:55 +0500

    set end get

--- interyu/index.js
+++ interyu/index.js
@@ -1,1 +1,14 @@
-decodeURI("%")
+const user = {
+    name: "Javohir",
+    fisrtName: "Suvonov",
+    get fullName() {
+        return this.name + " " + this.fisrtName
+    },
+
+    set fullName(value) {
+        [this.name, this.fisrtName] = value.split(" ")
+    }
+}
+
+user.fullName = "Ulug'bek Suvonov";
+console.log(user.fullName)
