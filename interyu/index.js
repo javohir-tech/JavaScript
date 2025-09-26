0c2cb8651bab04a9fb5e7f068f915f7fc0a414b0
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-27 00:05:24 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,31 +1,45 @@
 function getUsers(apiUrl) {
     return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest()
+        const request = new XMLHttpRequest();
 
-        request.addEventListener('readystatechange', () => {
-            // console.log(request, request.readyState)
+        request.addEventListener("readystatechange", () => {
             if (request.readyState === 4 && request.status === 200) {
                 const data = JSON.parse(request.responseText)
                 resolve(data)
             } else if (request.readyState === 4) {
-                reject("Ma'lumot olishni iloji bo'lmadi ")
+                reject("Ma'lumotlarni olishni iloji bo'lmadi")
             }
         })
 
-        request.open('GET', apiUrl)
+        request.open("GET", apiUrl);
 
         request.send()
     })
-
 }
 
-getUsers("./Todos/user.json")
+getUsers("https://jsonplaceholder.typicode.com/users")
     .then(res => {
         console.log(res)
     })
-    .catch(err => {
+    .catch(err => [
         console.log(err)
-    })
+    ])
+
+// const request = new XMLHttpRequest();
+
+// request.addEventListener("readystatechange" ,()=>{
+//     console.log(request , request.readyState)
+// })
+
+// request.open('GET' , "https://jsonplaceholder.typicode.com/users");
+
+// request.send()
+
+
+// 1
+// 2 open yuborildi
+// 3 loading
+// 4 tayyor
 
 // getUsers("./Todos/user.json", (data, err) => {
 //     console.log(data, "user")
