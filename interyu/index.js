6fe2b4352bc28ce7cc957d463796a602e8b7e49c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-27 01:07:24 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,57 +1,17 @@
-function getUser(apiUrl) {
-    return new Promise((resolve, reject) => {
-        const request = new XMLHttpRequest();
-
-        request.addEventListener('readystatechange', () => {
-            if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText)
-                resolve(data)
-            } else if (request.readyState === 4) {
-                reject("Ma'lumot olishni iloji bo'lmadi ")
-            }
-        });
-
-        request.open("GET", apiUrl);
-
-        request.send();
-    })
-
+function getuser() {
+    return new Promise((resolve, reject) => [
+        reject("Reject qabul qilindi va catch blok buni tutip oldi ")
+    ])
 }
 
-
-// async/await
-
-async function showUsers() {
+async function run() {
     try {
-        const user = await getUser("https://jsonplaceholder.typicode.com/users");
-        console.log(user)
+        // mana shu yerda await promise kutadi reject qabul qilgandan song kodni sinxron qilip yuboradi
+        const data = await getuser();
+        console.log(data)
     } catch (error) {
         console.log(error)
     }
 }
 
-showUsers()
-
-//then catch
-// getUser("https://jsonplaceholder.typicode.com/users")
-//     .then(res => {
-//         console.log(res)
-//     })
-//     .catch(err => {
-//         console.log(err)
-//     })
-//Calback hell
-// getUser("https://jsonplaceholder.typicode.com/users", (res, err) => {
-//     console.log(res)
-//     getUser("./Todos/javohir.json", (res, err) => {
-//         console.log(res);
-//         getUser("./Todos/suvonov.json", (res, err) => {
-//             console.log(res)
-//             getUser("./Todos/user.json", (res, err) => {
-//                 console.log(res)
-//             })
-//         })
-//     })
-// })
-
-
+run()
