ea3d5e550780d385069d0783bede306926053336
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-27 00:40:36 +0500

    promise

--- interyu/index.js
+++ interyu/index.js
@@ -1,68 +1,45 @@
-function getUsers(apiUrl) {
+function getUser(apiUrl) {
     return new Promise((resolve, reject) => {
         const request = new XMLHttpRequest();
 
-        request.addEventListener("readystatechange", () => {
+        request.addEventListener('readystatechange', () => {
             if (request.readyState === 4 && request.status === 200) {
                 const data = JSON.parse(request.responseText)
                 resolve(data)
             } else if (request.readyState === 4) {
-                reject("Ma'lumotlarni olishni iloji bo'lmadi")
+                reject("Ma'lumot olishni iloji bo'lmadi ")
             }
-        })
+        });
 
         request.open("GET", apiUrl);
 
-        request.send()
+        request.send();
     })
+
 }
 
-console.log(1)
-console.log(2)
-// getUsers("https://jsonplaceholder.typicode.com/users")
+
+
+//then catch
+// getUser("https://jsonplaceholder.typicode.com/users")
 //     .then(res => {
 //         console.log(res)
 //     })
-//     .catch(err => [
+//     .catch(err => {
 //         console.log(err)
-//     ])
-
-async function showusers() {
-    try {
-        const data = await getUsers("https://jsonplaceholder.typicode.com/users");
-        console.log(data)
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-showusers()
-console.log(3)
-console.log(4)
-
-// const request = new XMLHttpRequest();
-
-// request.addEventListener("readystatechange" ,()=>{
-//     console.log(request , request.readyState)
-// })
-
-// request.open('GET' , "https://jsonplaceholder.typicode.com/users");
-
-// request.send()
-
-
-// 1
-// 2 open yuborildi
-// 3 loading
-// 4 tayyor
-
-// getUsers("./Todos/user.json", (data, err) => {
-//     console.log(data, "user")
-//     getUsers("./Todos/javohir.json", (data, err) => {
-//         console.log(data, "javohir")
-//         getUsers("./Todos/suvonov.json", (data, err) => {
-//             console.log(data, "suvonov")
+//     })
+//Calback hell
+// getUser("https://jsonplaceholder.typicode.com/users", (res, err) => {
+//     console.log(res)
+//     getUser("./Todos/javohir.json", (res, err) => {
+//         console.log(res);
+//         getUser("./Todos/suvonov.json", (res, err) => {
+//             console.log(res)
+//             getUser("./Todos/user.json", (res, err) => {
+//                 console.log(res)
+//             })
 //         })
 //     })
 // })
+
 
