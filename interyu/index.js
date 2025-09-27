6428878ac9051b4474f5b4b39c0a98491fbca99c
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-28 00:40:12 +0500

    getUser function create

--- interyu/index.js
+++ interyu/index.js
@@ -1,7 +1,19 @@
-console.log(1); // call stack
+function getUser(apiUrl) {
 
-setTimeout(() => {
-    console.log(2) // wep api --> callback queue --> call stack
-})
+    return new Promise((resolve, reject) => {
+        const requst = new XMLHttpRequest();
 
-console.log(3) // call stack
+        requst.addEventListener("readystatechange", () => {
+            if (requst.readyState === 4 && requst.status === 200) {
+                const data = JSON.parse(requst.responseText);
+                resolve(data)
+            } else if (requst.readyState === 4) {
+                reject("Ma'lumot olsihni iloji bo'lmadi")
+            }
+        })
+
+        requst.open(apiUrl);
+
+        requst.send();
+    })
+}
