5d263029d1dd6537904d121c38935c6d64e3b94e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-29 20:33:18 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,3 +1,12 @@
-import { config } from "./main.js";
+// export an array
+export let months = ['Jan', 'Feb', 'Mar','Apr', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
-config.name = "Javohir"
+// export a constant
+export const MODULES_BECAME_STANDARD_YEAR = 2015;
+
+// export a class
+export class User {
+  constructor(name) {
+    this.name = name;
+  }
+}
