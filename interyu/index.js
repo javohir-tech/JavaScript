2a1af977fe2a3effe2c00e60366b780ef0f08ea7
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-09-30 21:51:39 +0500

    class

--- interyu/index.js
+++ interyu/index.js
@@ -1,12 +1,28 @@
-function Car(model, year) {
-    this.model = model;
-    this.year = year;
+// function Car(model, year) {
+//     this.model = model;
+//     this.year = year;
+// }
+
+// Car.prototype.signal = function () {
+//     console.log(`${this.model} signal chalayapti `)
+// }
+
+// const bmw = new Car('BMW', 2014);
+// console.log(bmw);
+// bmw.signal();
+
+class Car {
+    constructor(model, year) {
+        this.model = model;
+        this.year = year;
+    };
+
+    //signal bbu funksiya lekin class ichida method deyiladi
+    signal() {
+        console.log(`${this.model} signal chalayaptiiii`)
+    }
 }
 
-Car.prototype.signal = function () {
-    console.log(`${this.model} signal chalayapti `)
-}
-
-const bmw = new Car('BMW', 2014);
+const bmw = new Car("BMW", 2014);
 console.log(bmw);
 bmw.signal()
