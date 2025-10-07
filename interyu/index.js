4544b7a67e70d1622b35b4ea6f8364cfb961eac0
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-07 23:59:44 +0500

    callback hall

--- interyu/index.js
+++ interyu/index.js
@@ -1,38 +1,25 @@
-let open = indexedDB.open('mening bazam', 3);
+function getUser(url, callback) {
+    const request = new XMLHttpRequest()
 
-open.onupgradeneeded = (event) => {
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            callback(request.responseText)
+        } else if (request.readyState === 4) {
+            console.log('Malumotlarni olishni iloji bolmadi')
+        }
+    })
 
-    let db = event.target.result;
+    request.open('GET', url)
 
-    if (!db.objectStoreNames.contains('users')) {
-        db.createObjectStore('users', { keyPath: 'id', autoIncrement: true });
-        console.log('yangi baza va jadval yaratildi ')
-    }
+    request.send()
+
 }
+getUser('https://jsonplaceholder.typicode.com/users', (data) => {
+    console.log(data)
+    getUser('./Todos/javohir.json', (data) => {
+        console.log(data)
+    })
+})
 
-open.onsuccess = (event) => {
 
-    const db = event.target.result;
 
-    let transaction = db.transaction('users', 'readwrite');
-    let store = transaction.objectStore('users');
-
-    let user = {
-        name: 'Javohir',
-        age: 21
-    }
-
-    const request = store.add(user);
-
-    request.onsuccess = () => {
-        console.log('user storega qoshildi')
-    }
-
-    request.onerror = (event) => {
-        console.log(`xato : ${event.target.error}`)
-    }
-}
-
-open.onerror = (event) => {
-    console.log('Xato baza yaratilmadi : ', event.target.error)
-}
