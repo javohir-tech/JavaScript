84b75782478bf84f1654a3b6e137ce542667d31b
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-07 21:24:16 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,5 +1,12 @@
 localStorage.setItem('token', 123);
-localStorage.setItem('user', 'javohir')
-localStorage.setItem('email', 'suvonovjavohir625@gannil.com')
+localStorage.setItem('user', 'javohir');
+localStorage.setItem('email', 'suvonovjavohir625@gannil.com');
 
-console.log(localStorage.token);
+// console.log(localStorage.key(0))
+
+for (let i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    console.log(`${key}---${localStorage[key]}`)
+}
+
+// console.log(1)
