8c893884dc82b119090a7053efcc550e38671035
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-07 21:38:21 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,12 +1,25 @@
 localStorage.setItem('token', 123);
 localStorage.setItem('user', 'javohir');
-localStorage.setItem('email', 'suvonovjavohir625@gannil.com');
+localStorage.setItem('email', 'suvonovjavohir625@gmail.com');
 
 // console.log(localStorage.key(0))
 
-for (let i = 0; i < localStorage.length; i++) {
-    const key = localStorage.key(i);
-    console.log(`${key}---${localStorage[key]}`)
+// for (let i = 0; i < localStorage.length; i++) {
+//     const key = localStorage.key(i);
+//     console.log(`${key}---${localStorage[key]}`)
+// }
+
+// for(let key in localStorage){
+//     if(!localStorage.hasOwnProperty(key)){
+//         continue
+//     }
+//     console.log(key)
+// }
+
+const keys = Object.keys(localStorage);
+console.log(keys)
+for (let key in keys) {
+    console.log(`${keys[key]} ---- ${localStorage[keys[key]]}`)
 }
 
 // console.log(1)
