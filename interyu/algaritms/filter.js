427b1c21940a13610c7b42ead90697a53e4ea285
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-08 14:09:58 +0500

    update

--- interyu/algaritms/filter.js
+++ interyu/algaritms/filter.js
@@ -1,7 +1,7 @@
 var filter = function (arr, fn) {
     const result = []
-    for(let i = 0; i < arr.length; i++) {
-        if (fn(arr[i])) {
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
             result.push(arr[i])
         }
     }
@@ -12,4 +12,8 @@ function fn(n) {
     return n > 10
 }
 
-console.log(filter([0, 10, 20, 30] , fn))
+function firstIndex(n, i) {
+    return i === 0
+}
+
+console.log(filter([0, 10, 20, 30], firstIndex))
