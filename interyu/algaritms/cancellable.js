8bb824e21d7475ec72d17984efefd0ad6c308be5
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-08 17:35:01 +0500

    sleep

--- interyu/algaritms/cancellable.js
+++ interyu/algaritms/cancellable.js
@@ -4,28 +4,34 @@
  * @param {number} t
  * @return {Function}
  */
-var cancellable = function (fn, args, t) {
+function cancellable(fn, args, t) {
     let timer = setTimeout(() => {
         fn(args)
     }, t)
     return function cencalFn() {
         clearTimeout(timer)
     }
-};
-
-const result = []
-const fn = (x) => x * 5;
-const args = [2], t = 100, cancelTimeMs = 50
-const start = performance.now()
-const log = (...argsArr) => {
-    const diff = Math.floor(performance.now() - start);
-    result.push({"time": diff, "returned": fn(...argsArr)});
 }
-     
-const cancel = cancellable(log, args, t)
+
+const result = [];
+let start = performance.now()
+const fn = (x) => {
+    return x * 5;
+}
+
+const args = [2], t = 20, cancelTimeMs = 50;
+
+const log = (...args) => {
+    const diff = Math.floor(performance.now() - start);
+    result.push({ 'time': diff  , 'returned' : fn(...args)})
+}
+
+const cencel = cancellable(log, args, t);
+
 const maxT = Math.max(t, cancelTimeMs);
-         
-setTimeout(cancel, cancelTimeMs)
-setTimeout(() => {
-    console.log(result); // [{"time":20,"returned":10}]
-}, maxT + 15)
+
+setTimeout(cencel , cancelTimeMs);
+
+setTimeout(()=>{
+    console.log(result)
+}, maxT+15)
