b8cce1e7f6b564c3fb4ecfc4663bbccd81ec9503
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-08 14:56:41 +0500

    update

--- interyu/algaritms/compose.js
+++ interyu/algaritms/compose.js
@@ -1,7 +1,4 @@
-/**
- * @param {Function[]} functions
- * @return {Function}
- */
+
 var compose = function (functions) {
     return function (x) {   
         let result = x;
@@ -14,9 +11,3 @@ var compose = function (functions) {
 
 const fn = compose([x => x + 1, x => x * x, x => 2 * x]);
 console.log(fn(4))
-
-
-/**
- * const fn = compose([x => x + 1, x => 2 * x])
- * fn(4) // 9
- */
