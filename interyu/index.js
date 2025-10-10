c615244322f0e44acd9975eb40456d5dea48cd78
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-10 22:36:57 +0500

    update

--- interyu/index.js
+++ interyu/index.js
@@ -1,29 +1,24 @@
-function getUser(url) {
+function getUsers(url) {
     return new Promise((resolve, reject) => {
-
-        const request = new XMLHttpRequest()
-
+        const request = new XMLHttpRequest();
         request.addEventListener('readystatechange', () => {
             if (request.readyState === 4 && request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                resolve(data)
+                resolve(request.responseText)
             } else if (request.readyState === 4) {
-                reject('Malumotlarni olishni iloji bolmadi')
+                reject('Ma\'lumot olishni iloji bo\'lmadi')
             }
         })
 
-        request.open('GET', url)
-        
+        request.open('GET', url);
 
         request.send();
     })
-
 }
 
 const showUsers = async () => {
     try {
-        const res = await getUser('https://jsonplaceholder.typicode.com/users');
-        console.log(res)
+        const res = await getUsers('./Todos/suvonov.json');
+        console.log(res);
     } catch (error) {
         console.log(error)
     }
