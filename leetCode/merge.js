5b9de4b1c441303cf386fac5ad5c74185198cc6e
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-19 22:35:31 +0500

    update

--- leetCode/merge.js
+++ leetCode/merge.js
@@ -4,19 +4,24 @@
  * @return {string}
  */
 var mergeAlternately = function (word1, word2) {
-    const result = [];
-    let i = 0 ;
 
-    while (i < word1.length) {
-        result.push(word1[i]);
-        result.push(word2[i]);
-        i++
+    const result = [];
+    let i = 0;
+    const maxWord = Math.max(word1.length, word2.length);
+    while (i < maxWord) {
+        if (word1[i]) {
+            result.push(word1[i]);
+        }
+        if (word2[i]) {
+            result.push(word2[i]);
+        }
+        i++;
     }
 
-    console.log(result.join())
+    return result.join("")
 };
 
-const word1 = "bc";
+const word1 = "abc";
 const word2 = "pqr"
 
-mergeAlternately(word1, word2);
+console.log(mergeAlternately(word1, word2))
