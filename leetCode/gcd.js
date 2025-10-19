60f7b379b95bed3498aab61ad7aa1cfc528e2331
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-10-19 22:39:46 +0500

    update

---
+++ leetCode/gcd.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} str1
+ * @param {string} str2
+ * @return {string}
+ */
+var gcdOfStrings = function (str1, str2) {
+    
+};
+
+const str1 = "ABCABC"
+const str2 = "ABC"
+
+console.log(gcdOfStrings(str1, str2));
