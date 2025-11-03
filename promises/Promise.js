e9634e60af3b2073b5305972f7b8db1864dcebe1
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-03 17:52:34 +0500

    update

--- promises/Promise.js
+++ promises/Promise.js
@@ -2,9 +2,32 @@
 const p1 = new Promise((resolve, reject) => setTimeout(() => reject('p1'), 3000))
 const p2 = new Promise(resolve => setTimeout(() => resolve('p2'), 4000))
 
-Promise.allSettled([p1, p2])
+const promises = [p1, p2]
+
+Promise.all(promises)
     .then(results => {
         console.log(results)
-    }).catch(error=>{
-         console.log(`Hatolik : ${error}`)
+    }).catch(error => {
+        console.log(`Hatolik : ${error}`)
+    })
+
+Promise.race(promises)
+    .then(res => {
+        console.log(res)
+    }).catch(error => {
+        console.log(`xatolik ${error}`)
+    })
+
+Promise.any(promises)
+    .then(res => {
+        console.log(res)
+    }).catch(error => {
+        console.log(`Xatolik : ${error}`)
+    })
+
+Promise.allSettled(promises)
+    .then(res => {
+        console.log(res)
+    }).catch(error => {
+        console.log(`Xatolik : ${error}`)
     })
