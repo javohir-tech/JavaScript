bbbff1a11d4d6e1bbf31aad37e8fb82153a18703
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-05 15:45:11 +0500

    update

--- leetCode/gcd.js
+++ leetCode/gcd.js
@@ -4,13 +4,14 @@
  * @return {string}
  */
 var gcdOfStrings = function (str1, str2) {
+    if(str1+str2!==str2+str1) return ""
     const a = str1.length;
     const b = str2.length;
     const gcd = (a, b) => b === 0 ? a : gcd(b, a % b)
     const len = gcd(a, b)
-    return len
+    return str1.slice(0, len)
 };
 
-const str1 = "ABCABC"
-const str2 = "ABC"
+const str1 = "LEET"
+const str2 = "CODE"
 console.log(gcdOfStrings(str1, str2))
