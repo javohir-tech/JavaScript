9fd047ca4d00bd1bcf3375d9e30c978a8f071f30
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-05 16:19:14 +0500

    flowers

--- leetCode/flowers.js
+++ leetCode/flowers.js
@@ -4,23 +4,24 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function (flowerbed, n) {
-    const miniBed = flowerbed.slice(0, 3)
     let count = 0;
-    let i = 1
-    while (i < flowerbed.length - 2) {
-        if (miniBed.every(item => item === 0)) {
-            miniBed[1] = 1;
+    for (let i = 0; i < flowerbed.length; i++) {
+        if (
+            flowerbed[i] === 0 &&
+            (i === 0 || flowerbed[i - 1] === 0) &&
+            (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+        ) {
+            flowerbed[i] = 1;
             count++
-        } 
-        miniBed.shift();
-        miniBed.push(flowerbed[i + 2]);
-        console.log(miniBed)
-        i++
+            if (count >= n) return true
+        }
     }
-    console.log(count)
-    return n === count
+
+    return count >= n
 };
 
-const flowerbed = [0, 0, 1, 0, 1, 0, 0], n = 2
+const flowerbed = [0, 0, 1, 0, 1, 0, 0]
+// [1, 0 , 0, 0, 1] n=1
+const n = 2
 
 console.log(canPlaceFlowers(flowerbed, n))
