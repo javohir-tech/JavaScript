86f28851dace62a10be011583b6e929250ea53c2
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-11-12 19:01:52 +0500

    update

--- OOP/avto.js
+++ OOP/avto.js
@@ -1,5 +1,6 @@
 class Avto {
     #km;
+    static avto_num = 0
     constructor(make, model, rang, yil, narx, km = 0) {
         this.make = make;
         this.model = model;
@@ -7,7 +8,10 @@ class Avto {
         this.yil = yil;
         this.narx = narx;
         this.#km = km
+        Avto.avto_num ++
     }
+
+
 
     get_km() {
         return this.#km
@@ -22,6 +26,9 @@ class Avto {
     }
 }
 
-const avto1 = new Avto("GM", "Gentra", "qora", 2020, 12000, 1000);
-avto1.add_km(-3000)
-console.log(avto1.get_km())
+const m1 = new Avto("Chevrolet", "Cobalt", "oq", 2022, 15000);
+const m2 = new Avto("BMW", "X5", "qora", 2020, 40000);
+const m3 = new Avto("Kia", "K5", "kulrang", 2023, 30000);
+m1.add_km(3000)
+console.log(m1.get_km())
+console.log(Avto.avto_num)
