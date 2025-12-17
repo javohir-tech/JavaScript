6aa09712bebc6197be6d2183c44ea332f8dd3ad0
Suvonov Javohir <suvonovjavohir625@gmail.com>
2025-12-17 22:33:47 +0500

    update

---
+++ interyu/src/components/child.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <button type="button" @click="emit('increment')">+</button>
+    </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['increment'])
+</script>
+
+<style scoped>
+    
+</style>
